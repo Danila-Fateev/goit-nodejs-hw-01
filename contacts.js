98c1c0ad002755a91b7ad4576ebf088143f97fec
Вадим Фатеев <fateevdanon@gmail.com>
2022-09-15 23:03:04 +0200

    step 3

--- contacts.js
+++ contacts.js
@@ -1,24 +1,39 @@
-const fs = require("fs");
+const fs = require("fs/promises");
 const path = require("node:path");
+const shortid = require("shortid");
 
-const contactsPath = path.join(__dirname, "contacts.json");
+const contactsPath = path.join(__dirname, "db", "contacts.json");
 
-console.log(contactsPath);
+const listContacts = async () => {
+  const listOfContacts = await fs.readFile(contactsPath, "utf-8");
+  const parsedListOfContacts = JSON.parse(listOfContacts);
+  return parsedListOfContacts;
+};
 
-function listContacts() {
-  const listOfContacts = fs.readFile(contactsPath, "utf-8");
-  console.log(listOfContacts);
-  return JSON.parse(listOfContacts);
-}
-
-function getContactById(contactId) {
-  const listOfContacts = listContacts();
+const getContactById = async (contactId) => {
+  const listOfContacts = await listContacts();
   const contactFoundById = listOfContacts.find((item) => item.id === contactId);
-}
+  await fs.writeFile(contactsPath, JSON.stringify(listOfContacts, null, 2));
+  return contactFoundById;
+};
 
-function removeContact(contactId) {}
+const removeContact = async (contactId) => {
+  const listOfContacts = await listContacts();
+  const contactIndex = listOfContacts.findIndex(
+    (item) => item.id === contactId
+  );
+  listOfContacts.splice(contactIndex, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(listOfContacts, null, 2));
+  return listOfContacts;
+};
 
-function addContact(name, email, phone) {}
+const addContact = async (name, email, phone) => {
+  const listOfContacts = await listContacts();
+  const newContact = { id: shortid.generate(), name, email, phone };
+  listOfContacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(listOfContacts, null, 2));
+  return listOfContacts;
+};
 
 module.exports = {
   listContacts,
--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hello!");
+const contactsFunction = require("./contacts");
+
+const fn = async () => {
+  const data = await contactsFunction.addContact(
+    "John Martin",
+    "demon@gmail.com",
+    "+380 48 348 34"
+  );
+
+  console.log(data);
+};
+
+fn();
